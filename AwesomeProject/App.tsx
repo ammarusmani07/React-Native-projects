4d8db037914d8e454546056666fc92e5f424fa15
Admin <ammar.usmani777@gmail.com>
2024-06-23 05:38:15 -0700

    AwesomeProject directory and its contents

---
+++ AwesomeProject/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Text ,
+  SafeAreaView,
+  View
+} from 'react-native' 
+function App() {
+ return(
+  <SafeAreaView>
+  <View>
+    <Text>
+      hello world
+      
+    </Text>
+  </View>
+  <View>
+    <Text>
+      how are you 
+    </Text>
+  </View>
+</SafeAreaView>
+ )
+
+}
+export default App;
